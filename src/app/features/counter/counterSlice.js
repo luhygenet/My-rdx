0298ffe9894376d390a934ede2f2f20a83db90a0
Liya Daniel <luhygenet@gmail.com>
2025-09-22 00:17:24 +0300

    added counter reducer we made to reducer in store

--- src/app/features/counter/counterSlice.js
+++ src/app/features/counter/counterSlice.js
@@ -1,6 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
-    name: "counter",
-    initialState
-})
+export const counter = createSlice({
+  name: "counter",
+  initialState: { count: 0 },
+  reducers: {
+    //though we are directly accessing and mutating state (which is not allowed because compare to rerender will be the same even with change)
+    // but the immer library bts, so it's actually crating a new immutable obj
+    increment(curState) {
+      curState.count += 1;
+    },
+    decrement(curState) {
+      curState.count -= 1;
+    },
+  },
+});
+
+export const {increment,decrement} = counter.actions
+export default counterSlice.reducer
