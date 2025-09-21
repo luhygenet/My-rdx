23fe65dd402c80b68028bfee2f83e667b8c41bc3
Liya Daniel <luhygenet@gmail.com>
2025-09-21 23:51:19 +0300

    wrapped with provider, basically provides store

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById("root")).render(<App />);
-
+import store from "./app/source.js";
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
